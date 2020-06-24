d78a82c6b518c06534f8c12e6fe2ab3559a45684
Annemiek <annemiekf@team-x.nl>
2020-06-24 17:34:58 +0200

    Added the builder pattern to currency2 so it is now one with and one without the pattern.

--- src/main/java/CryptoCurrency/model/CurrencyRepository2.java
+++ src/main/java/CryptoCurrency/model/CurrencyRepository2.java
@@ -1,17 +1,17 @@
 package CryptoCurrency.model;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+/** This interface extends Spring Data PagingAndSortingRepository, specifying the domain type as Currency2 and the id type as String.
+ * This interface, though empty on the surface, supports:
+ *
+ * Creating new instances
+ * Updating existing ones
+ * Deleting
+ * Finding (one, all, by simple or complex properties)
+ *
+ * Spring Data’s repository solution makes it possible to sidestep data store specifics and
+ * instead solve a majority of problems using domain-specific terminology.
+ */
 
 public interface CurrencyRepository2 extends PagingAndSortingRepository<Currency2, String> {
-    /** This interface extends Spring Data PagingAndSortingRepository, specifying the domain type as Currency2 and the id type as String.
-     * This interface, though empty on the surface, supports:
-     *
-     * Creating new instances
-     * Updating existing ones
-     * Deleting
-     * Finding (one, all, by simple or complex properties)
-     *
-     * Spring Data’s repository solution makes it possible to sidestep data store specifics and
-     * instead solve a majority of problems using domain-specific terminology.
-     */
 }
